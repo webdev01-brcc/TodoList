be4ce63738a1cff130495770c9b73616179e4ee1
WEBDEV01 <webdev01@Alfreds-MacBook-Pro.local>
2022-04-11 02:56:08 -0500

    Corrected the item focus. Now able to add todo list to the todo list table.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 //Variables
 const ocAddTodoItem = document.getElementById('ocAddTodoItem')
+const ocAddTodoTitle = document.getElementById('ocAddTodoTitle')
+const ocAddTodoSaveButton = document.getElementById('ocAddTodoSaveButton')
 const ocAddTodoItemsList = document.getElementById('ocAddTodoItemsList')
+const todoListBody = document.getElementById('todoListBody')
 let ocAddTodoItems = []
+let todos = []
 
 //HTML - Templates
 const todoItem = (id, value) => {
@@ -29,19 +33,50 @@ const todoItem = (id, value) => {
     `
 }
 
-//Functions
-const mainObj = (id, value) => {
+const todoListItem = (todo, index) => {
+    const completes = todo.items.map(i => i.complete)
+
+    const status = completes.every(i => i == true)
+        ? 'Complete'
+        : completes.every(i => i == false)
+            ? 'Pending'
+            : 'Incomplete'
+
+    return `<tr>
+        <th scope="row">${index + 1}</th>
+        <td>${todo.title}</td>
+        <td>${status}</td>
+        <td>${dayjs(todo.id).format('MMM DD, YYYY')}</td>
+        <td align="right">
+            <button type="button" class="btn btn-secondary btn-sm mr-1">Edit</button>
+            <button type="button" class="btn btn-danger btn-sm">Remove</button>
+        </td>
+    </tr>
+    `
+}
+
+//HELPER FUNCTIONS
+const mainItemObj = (id, value) => {
     return {
         id,
         value,
+        complete: false,
         html: todoItem(id, value)
     }
 }
+
+const bringInputIntoFocus = (index) => {
+    const input = ocAddTodoItemsList.children.item(index).children.item(0)
+    input.focus()
+    input.setSelectionRange(input.value.length, input.value.length)
+}
+
+//ADD TODO
 const addItem = (e) => {
     const itemId = Date.now()
 
     //Main Object
-    const item = mainObj(itemId, '')
+    const item = mainItemObj(itemId, '')
 
     ocAddTodoItems.push(item)
     ocAddTodoReplaceItemsList()
@@ -60,19 +95,53 @@ const ocAddTodoReplaceItemsList = () => {
 const ocAddTodoItemInputContent = (e) => {
     const value = e.target.value
     const id = e.target.id.split('-')[1]
+    const index = ocAddTodoItems.findIndex(i => i.id == id)
 
     ocAddTodoItems = ocAddTodoItems.map(i => {
         if (i.id == id) {
-            return mainObj(id, value)
+            return mainItemObj(id, value)
         }
 
         return i
     })
 
     ocAddTodoReplaceItemsList()
+    bringInputIntoFocus(index)
 }
+
+const ocAddTodoSave = () => {
+    const id = Date.now()
+    const title = ocAddTodoTitle.value
+
+    if (title == '') {
+        return alert('Must have a title.')
+    }
+
+    if (!ocAddTodoItems.length) {
+        return alert(`Must have at least one item in the ${title} todo list.`)
+    }
+
+    todos.push({
+        id,
+        title,
+        items: ocAddTodoItems
+    })
+
+    ocAddTodoTitle.value = ''
+    ocAddTodoItems = []
+    ocAddTodoReplaceItemsList()
+    populateTheList()
+}
+
+//MAIN TODO LIST
+const populateTheList = () => {
+    const html = todos.map((todo, index) => todoListItem(todo, index))
+    todoListBody.innerHTML = html.join('')
+}
+
 
 //DOM Listen
 window.addEventListener('DOMContentLoaded', () => {
     ocAddTodoItem.addEventListener('click', addItem)
+    ocAddTodoSaveButton.addEventListener('click', ocAddTodoSave)
 })
